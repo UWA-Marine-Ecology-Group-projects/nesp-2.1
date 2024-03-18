846b136c361d74fdf7baab248e4242234a86b9e9
claudehspencer <claudespencer111@gmail.com>
2024-03-19 07:47:44 +0800

    working on scatterpies

--- R/04_report-figures/01_scatterpies.R
+++ R/04_report-figures/01_scatterpies.R
@@ -41,14 +41,10 @@ hab_fills <- scale_fill_manual(values = c("Sessile invertebrates" = "plum",
                                           "Sand" = "wheat"), 
                                name = "Habitat")
 
-# Set cropping extent - larger than most zoomed out plot
-# e <- ext(118, 125, -35, -33)
-
 # Load necessary spatial files
 # Australian outline and state and commonwealth marine parks
 aus    <- st_read("data/spatial/shapefiles/aus-shapefile-w-investigator-stokes.shp") %>%
   st_transform(4326)
-# ausc <- st_crop(aus, e)
 
 # Read in bathymetry data
 bathy <- rast("data/spatial/rasters/raw bathymetry/Australian_Bathymetry_and_Topography_2023_250m_MSL_cog.tif") %>%
@@ -68,12 +64,74 @@ hill <- rast("data/spatial/rasters/raw bathymetry/250m_australia_hillshade.tif")
 pal_greys <- hcl.colors(1000, "Grays")
 
 # Load marine parks
-marine.parks <- st_read("data/spatial/shapefiles/Collaborative_Australian_Protected_Areas_Database_(CAPAD)_2022_-_Marine.shp") %>%
+marine.parks <- st_read("data/spatial/shapefiles/Australian_Marine_Parks.shp") %>%
   glimpse()
-plot(marine.parks["ZONE_TYPE"])
+plot(marine.parks["ZONENAME"])
 
-habitat_sf <- st_as_sf(habi, coords = c("longitude", "latitude"), crs = 4326,
-                       remove = F)
+unique(marine.parks$ZONENAME)
+mpa_fills <- scale_fill_manual(values = c("National Park Zone" = "#7bbc63",
+                                          "Habitat Protection Zone" = "#fff8a3",# Commonwealth MPA colours
+                                          # "Habitat Protection Zone (Reefs)" = "#fbff85",
+                                          "Multiple Use Zone" = "#b9e6fb",
+                                          # "Recreational Use Zone" = "#ffb36b",
+                                          # "Sanctuary Zone" = "#f7c0d8",
+                                          "Special Purpose Zone (Mining Exclusion)" = "#368ac1",
+                                          # "Special Purpose Zone (Trawl)" = "#3e8ec4",
+                                          "Special Purpose Zone" = "#6daff4"
+))
+
+# APOLLO
+habitat_park <- dplyr::filter(habi, amp %in% "Apollo")
+min(habitat_park$longitude)
+max(habitat_park$longitude)
+min(habitat_park$latitude)
+max(habitat_park$latitude)
+
+e <- ext(143.4725, 143.6833,-39.0183, -38.8312)
+
+hillshadec <- terra::crop(hill, e)
+topoc <- terra::crop(topo, e)
+bathyc <- terra::crop(bathy, e)
+ausc <- st_crop(aus, e)
+
+png("plots/report/apollo_scatterpie.png", height = 7, width = 9,
+    res = 900, units = "in")
+ggplot() +
+  geom_spatraster(data = hillshadec, alpha = 1, show.legend = F, maxcell = Inf) +
+  scale_fill_gradientn(colors = pal_greys, na.value = NA) +
+  new_scale_fill() +
+  geom_spatraster(data = bathyc, maxcell = Inf, alpha = 0.6) +
+  scale_fill_gradientn(colours = c("#061442","#014091", "#2b63b5","#6794d6"),
+                       values = rescale(c(-100, -40,-20, 0)),
+                       na.value = "#A0C3D8", name = "Depth")  +
+  new_scale_fill() +
+  geom_sf(data = ausc) +
+  geom_sf(data = marine.parks, aes(fill = ZONENAME), alpha = 0.3, colour = NA) +
+  scale_fill_manual(values = c("Multiple Use Zone" = "#b9e6fb"), 
+                    name = "Australian Marine Parks") +
+  new_scale_fill() +
+  geom_point(data = habitat_park, aes(x = longitude, y = latitude),
+             fill = "white", colour = "white", alpha = 0.1, size = 7, shape = 16) +
+  geom_scatterpie(data = habitat_park, aes(x = longitude, y = latitude),
+                  cols = c("Sessile invertebrates", "Macroalgae", "Seagrass", "Rock", "Sand"),
+                  colour = NA, pie_scale = 1) +
+  hab_fills +
+  labs(x = "Longitude", y = "Latitude") +
+  coord_sf(xlim = c(143.4725, 143.65), 
+           ylim = c(-39, -38.85), 
+           crs = 4326) +
+  theme_minimal() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
+dev.off()
+
+
+
+
+
+
+
+
 
 crop_and_scatterpie <- function(hillshade, bathymetry, topography, data, park.name) {
   
